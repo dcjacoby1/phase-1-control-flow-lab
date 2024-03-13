17c10db586f46b9ab7119627b64ec88f933720f2
Dan Jacoby <jacobydan01@gmail.com>
2024-03-12 21:43:53 -0400

    finished

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400){
+    return ('This one is on me!');
+  } else if (someValue > 400 && someValue <= 2000){
+    return ('That will be twenty bucks.');
+  } else if (someValue > 2000 && someValue <= 2500){
+    return ('I will gladly take your thirty bucks.');
+  } else {
+    return ('No can do.');
+  }
+}
+function ternaryCheckCity(someValue) {
+  return someValue === "NYC" ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
+
+function switchOnCharmFromTip(someValue){
   // Write your code here!
+    let response;
+  switch (someValue){
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+      default:
+        response = 'Bye.'
+
+  }
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
